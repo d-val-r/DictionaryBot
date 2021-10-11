1f079347e2ede7c10e227065666ad42552e245b3
d-val-r <dvrudenya@noctrl.edu>
2021-10-10 21:31:34 -0500

    test pandas dataframe approach

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import os, requests
+import os, requests, pandas
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -7,8 +8,11 @@ dict_key = os.getenv("DICT_KEY")
 thes_key = os.getenv("THES_KEY")
 word = input("enter a word: ")
 
-raw = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}")
+raw = str(requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}").text)
 
-response = raw.json()
 
-print(response)
+json_data = pandas.read_json(raw)
+
+df = pandas.DataFrame(json_data)
+
+
