b083a110691b717087af244d00aeb24e27505b19
d-val-r <dvrudenya@noctrl.edu>
2021-10-10 22:21:03 -0500

    add discord functionality

--- bot.py
+++ bot.py
@@ -1,25 +1,40 @@
 import os, requests 
 from dotenv import load_dotenv
 from pandas import DataFrame, read_json
+from discord.ext import commands
 
 
 load_dotenv()
 
 dict_key = os.getenv("DICT_KEY")
 thes_key = os.getenv("THES_KEY")
-word = input("enter a word: ")
+disc_key = os.getenv("DISCORD_KEY")
 
-try:
-    raw = str(requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}").text)
+client = commands.Bot(command_prefix = "#")
+
+def request_def(word):
+    response = []
+    try:
+        raw = str(requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}").text)
+
+        json_data = read_json(raw)
+
+        df = DataFrame(json_data)
+
+        for definition in df['shortdef']:
+            for part in definition:
+                response.append(part)
+            response.append("\n")
+            
+    except:
+        response.append("Merriam-Webster has no entry for this word.")
+    return response
 
 
-    json_data = read_json(raw)
+@client.command(pass_context = True)
+async def define(ctx, word):
+    for response in request_def(word):
+        await ctx.channel.send(response)
 
-    df = DataFrame(json_data)
 
-    for definition in df['shortdef']:
-        for parts in definition:
-            print(parts)
-        print("\n\n", end="")
-except:
-    print("Merriam-Webster has no entry for this word.")
+client.run(disc_key)
