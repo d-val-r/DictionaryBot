e6a74361a2014f28690764cd3a942cade43b6b2f
d-val-r <dvrudenya@noctrl.edu>
2021-10-10 22:05:50 -0500

    access short defs of API return value

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-import os, requests, pandas
+import os, requests 
 from dotenv import load_dotenv
+from pandas import DataFrame, read_json
 
 
 load_dotenv()
@@ -8,11 +9,17 @@ dict_key = os.getenv("DICT_KEY")
 thes_key = os.getenv("THES_KEY")
 word = input("enter a word: ")
 
-raw = str(requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}").text)
+try:
+    raw = str(requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}").text)
 
 
-json_data = pandas.read_json(raw)
+    json_data = read_json(raw)
 
-df = pandas.DataFrame(json_data)
+    df = DataFrame(json_data)
 
-
+    for definition in df['shortdef']:
+        for parts in definition:
+            print(parts)
+        print("\n\n", end="")
+except:
+    print("Merriam-Webster has no entry for this word.")
