ef2168446d505696ed7340ce9401249374eb115b
d-val-r <dvrudenya@noctrl.edu>
2021-10-10 19:27:47 -0500

    add requests functionality

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
-import os
+import os, requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 dict_key = os.getenv("DICT_KEY")
 thes_key = os.getenv("THES_KEY")
+word = input("enter a word: ")
 
-print(dict_key, thes_key)
+raw = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}")
+
+response = raw.json()
+
+print(response)
