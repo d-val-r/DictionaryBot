14b344ad7c075ab719852250b1a2ef1358e1f5ce
d-val-r <dvrudenya@noctrl.edu>
2021-10-12 22:11:04 -0500

    add spell check functionality

--- bot.py
+++ bot.py
@@ -6,34 +6,41 @@ from discord.ext import commands
 
 load_dotenv()
 
+# get API keys from environment variables
+disc_key = os.getenv("DISCORD_KEY")
 dict_key = os.getenv("DICT_KEY")
 thes_key = os.getenv("THES_KEY")
-disc_key = os.getenv("DISCORD_KEY")
+thes_url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key={}"
+dict_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}"
 
-client = commands.Bot(command_prefix = "#")
-
-def request_def(word):
+# the main function that will handle user requests
+def request_word(word, url, key):
     response = []
+    raw = str(requests.get(url.format(word, key)).text)
+
+    json_data = read_json(raw)
+
+    df = DataFrame(json_data)
     try:
-        raw = str(requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={dict_key}").text)
-
-        json_data = read_json(raw)
-
-        df = DataFrame(json_data)
-
         for definition in df['shortdef']:
             for part in definition:
                 response.append(part)
             response.append("\n")
-            
+
     except:
-        response.append("Merriam-Webster has no entry for this word.")
+        try:
+            for index in df.index:
+                response.append(f"Did you mean {df[0][index]}?")
+        except:
+            response.append("Merriam-Webster has no entry for this word.")
     return response
 
 
+# start of the bot-related code
+client = commands.Bot(command_prefix = "#")
 @client.command(pass_context = True)
 async def define(ctx, word):
-    for response in request_def(word):
+    for response in request_word(word, dict_url, dict_key):
         await ctx.channel.send(response)
 
 
