26023a322a1df040d52da02dad181e0208baf221
d-val-r <dvrudenya@noctrl.edu>
2021-10-13 07:50:51 -0500

    limit number of suggested spelling returns to 10

--- bot.py
+++ bot.py
@@ -13,24 +13,41 @@ thes_key = os.getenv("THES_KEY")
 thes_url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/{}?key={}"
 dict_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/{}?key={}"
 
-# the main function that will handle user requests
+# the main function that will handle user requests, parameters are the word
+# to look up, the API url (dictoinary vs thesaurus), and the associated API key
 def request_word(word, url, key):
     response = []
+
+    # transform the API return value into a managable format
     raw = str(requests.get(url.format(word, key)).text)
 
     json_data = read_json(raw)
 
     df = DataFrame(json_data)
+
+    # short definitions are located at the bottom of the dataframe
+    # in a cell labeled 'shortdef'
     try:
         for definition in df['shortdef']:
             for part in definition:
                 response.append(part)
             response.append("\n")
 
+    # the user entered a mispelled word or a word that doesn't exist
     except:
+
+        # in the case of a misspelled word, the return value is a dataframe
+        # of suggested spellings
         try:
+
+            # find all suggested spellings, truncate to 10 entries if
+            # too large
             for index in df.index:
                 response.append(f"Did you mean {df[0][index]}?")
+            if len(response) > 10:
+                response = response[:10]
+
+        # there are no suggested spellings
         except:
             response.append("Merriam-Webster has no entry for this word.")
     return response
